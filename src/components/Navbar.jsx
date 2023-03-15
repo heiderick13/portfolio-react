9a4697636791749e54bdaed2a8e55a32a3675ca0
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-15 18:00:17 -0300

    :tada: initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "../styles/main.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Logo</h2>
+      <div>
+        <a>home</a>
+        <a>about</a>
+        <a>shop</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
