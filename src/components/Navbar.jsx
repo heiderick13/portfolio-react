69543b7517604ed5bd7a4afbdf17d7442f4bdbef
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-16 17:11:51 -0300

    :lipstick: feat: navbar ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
 import "../styles/main.css";
+import logo from "../assets/img/logo-mobile.png";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <h2>Logo</h2>
-      <div>
-        <a>home</a>
-        <a>about</a>
-        <a>shop</a>
-      </div>
-    </nav>
+    <header>
+      <img className="myLogo" src={logo} alt="" />
+      <nav className="menu">
+        <ul className="menu__lista">
+          <li className="menu__item">
+            <a className="menu__link" href="index.html">
+              Home
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#projetos">
+              Projetos
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#habilidades">
+              habilidades
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
