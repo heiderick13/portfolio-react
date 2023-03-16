73619882a21888b27b04d7d88305d5f55b43ae84
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-16 19:33:00 -0300

    :construction: creating footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/heiderick13">
+          <i className="bi bi-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/marcelo-ferreira-de-oliveira">
+          <i className="bi bi-linkedin"></i>
+        </a>
+
+        <p>&copy; Marcelo Oliveira</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
