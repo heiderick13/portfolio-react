5e92e7b0af01e328d4f655196995fafd0ee7a9b4
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-17 11:21:51 -0300

    :sparkles: feat: new section - about me

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,20 @@
+import "./AboutMe.css";
+import profile from "../../assets/img/pfp.png";
+
+function AboutMe() {
+  return (
+    <section className="about">
+      <h2 className="about__title">Quem sou eu</h2>
+      <div className="about__info">
+        <img className="about__img" src={profile} alt="Marcelo sorrindo" />
+        <p>
+          Olá, eu sou o Marcelo! Nascido e criado na Zona Norte do Rio de
+          Janeiro, apaixonado por futebol. Atuo como desenvolvedor Frontend, na
+          construção de páginas web e sites dinâmicos.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Navbar from "./components/Navbar/Navbar";
+import AboutMe from "./components/AboutMe/AboutMe";
 import Footer from "./components/Footer/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
+    <AboutMe />
     <App />
     <Footer />
   </React.StrictMode>
