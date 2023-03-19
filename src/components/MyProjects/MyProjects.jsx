3add2630592ebe78c797f8129265fd70a92c8647
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-19 17:11:15 -0300

    :sparkles: feat: add real projects

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,7 +1,11 @@
 import "./MyProjects.css";
 import Card from "../Card/Card";
-import project1 from "../../assets/img/pfp.png";
-import project2 from "../../assets/img/pfp.png";
+import calculator from "../../assets/img/calculator.png";
+import ecommerce from "../../assets/img/ecommerce-product.png";
+import shortUrl from "../../assets/img/shortening-url.png";
+import geniusGame from "../../assets/img/genius-game.png";
+import weather from "../../assets/img/weather-app.png";
+import todoApp from "../../assets/img/todo-app.png";
 
 function MyProjects() {
   return (
@@ -10,18 +14,39 @@ function MyProjects() {
       <div className="projects__list">
         <Card
           name="Product Preview"
-          foto={project1}
+          foto={calculator}
           description="Página de preview de um perfume desenvolvida com HTML e CSS"
+          deploy={"https://heiderick13.github.io/calculator-oop/"}
         />
         <Card
           name="Página de Login"
-          foto={project2}
+          foto={ecommerce}
           description="Página de login com HTML e CSS"
+          deploy={"https://ecommerce-product-page-green.vercel.app/"}
         />
         <Card
-          name="Página de Login"
-          foto={project2}
-          description="Página de login com HTML e CSS"
+          name="Aplicativo Lista de Tarefas"
+          foto={todoApp}
+          description="Aplicação para controle de tarefas diárias. É possível adicionar e editar novas tarefas. marcá-las como concluídas e excluir."
+          deploy={"https://github.com/heiderick13/genius-game"}
+        />
+        <Card
+          name="Lading page com API"
+          foto={shortUrl}
+          description="Landing page de uma empresa fictícia de serviço onde usuário pode encurtar urls de forma rápida e fácil."
+          deploy={"https://heiderick13.github.io/shorten-url-landing-page/"}
+        />
+        <Card
+          name="Jogo Genius"
+          foto={geniusGame}
+          description="Clone do famoso jogo Genius. Usuário deve clicar nos botões na sequência correta."
+          deploy={"https://github.com/heiderick13/genius-game"}
+        />
+        <Card
+          name="Weather App"
+          foto={weather}
+          description="Aplicativo de pesquisa do tempo ao redor do mundo onde o usuário pode buscar pelo nome de qualquer local que desejar."
+          deploy={"https://github.com/heiderick13/weather-app"}
         />
       </div>
     </section>
