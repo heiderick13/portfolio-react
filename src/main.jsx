20e3c80e5568c80d998d94f3f0a5d57cef10fc7d
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-20 18:03:32 -0300

    :heavy_plus_sign: build: add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import Home from "./routes/Home";
+import ContactPage from "./routes/ContactPage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contactme",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
