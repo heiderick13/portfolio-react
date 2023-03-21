4084af7b261cd23d95dd78c4be6b48ab7d96c6bf
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-21 11:16:33 -0300

    :sparkles: update: add new project and reorder

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -3,7 +3,7 @@ import Card from "../Card/Card";
 import calculator from "../../assets/img/calculator.png";
 import ecommerce from "../../assets/img/ecommerce-product.png";
 import shortUrl from "../../assets/img/shortening-url.png";
-import geniusGame from "../../assets/img/genius-game.png";
+import spaceTourism from "../../assets/img/space-tourism.png";
 import weather from "../../assets/img/weather-app.png";
 import todoApp from "../../assets/img/todo-app.png";
 
@@ -13,10 +13,10 @@ function MyProjects() {
       <h2 className="projects__title">Meus últimos projetos</h2>
       <div className="projects__list">
         <Card
-          name="Calculadora"
-          foto={calculator}
-          description="Calculadora desenvolvida com o paradigma de orientação a objetos, método muito popular e requisitado no mundo da programação."
-          deploy={"https://heiderick13.github.io/calculator-oop/"}
+          name="Turismo Espacial"
+          foto={spaceTourism}
+          description="Página de uma agência de turismo espacial. São apresentados os destinos, tripulação e tecnologias utilizadas na sua viagem ao espaço."
+          deploy={"https://travelthruspace.vercel.app/"}
         />
         <Card
           name="
@@ -26,28 +26,28 @@ function MyProjects() {
           deploy={"https://ecommerce-product-page-green.vercel.app/"}
         />
         <Card
-          name="Aplicativo Lista de Tarefas"
-          foto={todoApp}
-          description="Aplicação para controle de tarefas diárias. É possível adicionar e editar novas tarefas. marcá-las como concluídas e excluir."
-          deploy={"https://trackyourtasks.vercel.app/"}
-        />
-        <Card
           name="Lading page com API"
           foto={shortUrl}
           description="Landing page de uma empresa fictícia de serviço onde usuário pode encurtar urls de forma rápida e fácil."
           deploy={"https://heiderick13.github.io/shorten-url-landing-page/"}
         />
         <Card
-          name="Jogo Genius"
-          foto={geniusGame}
-          description="Clone do famoso jogo Genius. Usuário deve clicar nos botões na sequência correta."
-          deploy={"https://genius-game-gray.vercel.app/"}
+          name="Aplicativo Lista de Tarefas"
+          foto={todoApp}
+          description="Aplicação para controle de tarefas diárias. É possível adicionar e editar novas tarefas. marcá-las como concluídas e excluir."
+          deploy={"https://trackyourtasks.vercel.app/"}
         />
         <Card
           name="Weather App"
           foto={weather}
           description="Aplicativo de pesquisa do tempo ao redor do mundo onde o usuário pode buscar pelo nome de qualquer local que desejar."
           deploy={"https://searchtheweather.vercel.app/"}
+        />
+        <Card
+          name="Calculadora"
+          foto={calculator}
+          description="Calculadora desenvolvida com o paradigma de orientação a objetos, método muito popular e requisitado no mundo da programação."
+          deploy={"https://heiderick13.github.io/calculator-oop/"}
         />
       </div>
     </section>
